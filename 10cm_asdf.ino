6276b69f19837bfe1989f77e877b11140904b585
ProgramingJSH <97013845+ProgramingJSH@users.noreply.github.com>
2022-05-26 18:11:45 +0900

    Update 10cm_asdf.ino

--- 10cm_asdf.ino
+++ 10cm_asdf.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 #define BT_RXD 8
 #define BT_TXD 7
-SoftwareSerial bluetooth(BT_RXD, BT_TXD);
+SoftwareSerial Serial1(BT_RXD, BT_TXD);
 
 int trigPin = 11;
 int echoPin = 12;
@@ -9,7 +9,7 @@ int echoPin = 12;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  bluetooth.begin(9600);
+  Serial1.begin(9600);
   
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
@@ -25,10 +25,8 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   distance = ((float)(340*duration)/1000)/2;
 
-
-
   if (distance < 100) {
-    bluetooth.write("asdf");
+    Serial1.write("asdf");
     Serial.println("asdf");
   }
   delay(500);
