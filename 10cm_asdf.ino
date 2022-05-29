156a46a82b41d0161d99a2fd5a383af0846e9bc3
ProgramingJSH <97013845+ProgramingJSH@users.noreply.github.com>
2022-05-29 22:13:38 +0900

    Update 10cm_asdf.ino
    앱인벤터와 연결시에 참고할 파일.

--- 10cm_asdf.ino
+++ 10cm_asdf.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 #define BT_RXD 8
 #define BT_TXD 7
-SoftwareSerial Serial1(BT_RXD, BT_TXD);
+SoftwareSerial bluetooth(BT_RXD, BT_TXD);
 
 int trigPin = 11;
 int echoPin = 12;
@@ -9,7 +9,7 @@ int echoPin = 12;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial1.begin(9600);
+  bluetooth.begin(9600);
   
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
@@ -18,16 +18,29 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   long duration, distance;
+  char text = bluetooth.read()
+  
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
   distance = ((float)(340*duration)/1000)/2;
-
+  
   if (distance < 100) {
-    Serial1.write("asdf");
-    Serial.println("asdf");
+    bluetooth.println("방안에 누가 들어왔습니다!");
+    Serial.println('1');
   }
+  //if (bluetooth.available()){
+  //  if (text == 'L') { 
+  //  }
+  //  if (text == 'R') {
+  //  }
+  //  if (text == 'S') {
+  //  }
+  //  if (text == 'B') {
+  //  }
+  //}
+  
   delay(500);
 }
