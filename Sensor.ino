281ee3c53e6171cf794f26bb8fef10b785221482
ProgramingJSH <97013845+ProgramingJSH@users.noreply.github.com>
2022-05-30 12:37:47 +0900

    Update Sensor.ino

--- Sensor.ino
+++ Sensor.ino
@@ -11,18 +11,30 @@ int pirSensor 11 //인체감지 센서
 void setup() {
   Serial.begin(9600);
   Bluetooth.begin(9600);
-  pinMode (pirSensor, INPUT);             //  핀모드 센서 입력값으로 설정
+  pinMode (pirSensor, INPUT);                //  핀모드 센서 입력값으로 설정
 }
 
 void loop() {
-
+  
   if (digitalRead(pirSensor) == HIGH) {
-    Bluetooth.write("움직임이 관찰되었습니다."); //  휴대폰 화면에 올라가는 메시지
-    Serial.println("나가!");                //  아두이노 시리얼 모니터에 올라가는 메시지. 확인용.
+    Bluetooth.write("방안에 누가 들어왔습니다!");  //  휴대폰 화면에 올라가는 메시지
+    Serial.println("나가!");                  //  아두이노 시리얼 모니터에 올라가는 메시지. 확인용.
   }
   else {
-    Serial.println("후,..");               //  아두이노 시리얼 모니터에 올라가는 메시지. 확인용.
+    Serial.println("후,..");                 //  아두이노 시리얼 모니터에 올라가는 메시지. 확인용.
   }
+  
+  if (bluetooth.available()){
+    if (text == 'L') {
+    }
+    if (text == 'R') {
+    }
+    if (text == 'S') {
+    }
+    if (text == 'B') {
+    }
+  }
+  
   delay(500);
 }
 
